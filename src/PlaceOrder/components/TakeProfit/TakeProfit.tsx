b8a3d5085ccbc3ca9d42668c58a400913f8d20ce
Dzmitry Melnikau <dimaira2012@yandex.by>
2023-10-18 10:58:04 +0200

    Initial commit

---
+++ src/PlaceOrder/components/TakeProfit/TakeProfit.tsx
@@ -0,0 +1,10 @@
+/* eslint @typescript-eslint/no-use-before-define: 0 */
+import React from "react";
+
+import "./TakeProfit.module.scss";
+
+const TakeProfit = () => {
+  return <div>[Take Profit Block]</div>;
+};
+
+export { TakeProfit };
